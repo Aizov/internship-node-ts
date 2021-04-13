8a307a232245f386a09465695fac06ad97bc72bd
John Aizov <3x6death@gmail.com>
2021-04-13 11:14:01 +0300

    Created cashiers and cash registers methods

--- app.ts
+++ app.ts
@@ -8,6 +8,35 @@ interface ShopInterface {
 }
 
 
+let tables = [
+    {
+        tablename: 'Cashiers',
+        fields: {
+            id: 'integer',
+            name: 'varchar',
+            surname: 'varchar',
+            age: 'int'
+        }
+    },
+    {
+        tablename: 'Shops',
+        fields: {
+            id: 'integer',
+            name: 'varchar',
+            address: 'varchar'
+        }
+    },
+    {
+        tablename: 'CashRegisters',
+        fields: {
+            id: 'integer',
+            number: 'integer',
+            date: 'date',
+            timework: 'integer'
+        }
+    }
+]
+
 class App {
     private client: Pool
     private connected: boolean | undefined;
@@ -55,46 +84,132 @@ class App {
     }
     async checkTables (){
 
-        let tables = [
+
+        let shopsData = [
             {
-                tablename: 'Cashiers',
-                fields: {
-                    id: 'integer',
-                    name: 'varchar',
-                    surname: 'varchar',
-                    age: 'int'
-                }
+                id: 1, name: 'ATB', address: 'Vulicya 54',
             },
+            {
+                id: 2, name: 'Vilma', address: 'DJjjdjj2',
+            },
+            {
+                id: 3, name: 'Fora', address: 'Foravulica',
+            }
         ]
 
         let cashiersData = [
             {
-                id: 1,
-                name: 'Ivan',
-                surname: 'Surname',
-                age: 23,
+                id: 1, name: 'Ivan', surname: 'Surname', age: 23,
                 ExpShop_2: 3,
+                ExpShop_3: 1,
 
             },
             {
-                id: 2,
-                name: 'Vilma',
-                adress: 'DJjjdjj2',
+                id: 2, name: 'Andrei', surname: 'Serend', age: 41,
+                ExpShop_2: 3,
+                ExpShop_3: 2,
+            },
+            {
+                id: 3, name: 'Andreddddi', surname: 'Serxxxend', age: 35,
+                ExpShop_1: 4,
+                ExpShop_3: 1,
             }
         ]
 
-        let shopsData = [
-            {
-                id: 1,
-                name: 'ATB',
-                adress: 'Vulicya',
-            },
-            {
-                id: 2,
-                name: 'Vilma',
-                adress: 'DJjjdjj2',
+        async function pushCashiers() {
+            for (let i = 0; i < cashiersData.length; i++){
+                await app.dbQuery(makePull('Cashiers', cashiersData[i]), (res: any) => {
+                    console.log(makePull('Cashiers', cashiersData[i]))
+                    console.log(res)
+                    // cashiersData[i]
+                })
             }
-        ]
+        }
+        await pushCashiers()
+
+
+
+        async function pushCashRegistersData() {
+            const date = new Date();
+            function getRandomInt(min: number, max: number) {
+                min = Math.ceil(min); max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+
+            let cashRegistersData = [
+                {
+                    id: 1,
+                    number: 5,
+                    date: '2021-04-12T06:37:12.688Z',
+                    timework: 1
+                }
+            ]
+
+            for (let i = 1; i < getRandomInt(1, 40); i++) {
+
+                date.setDate(date.getDate() - getRandomInt(1, 31))
+                let dateStr = date.toISOString();
+
+                cashRegistersData = [...cashRegistersData, {
+                    id: cashRegistersData.length,
+                    number: i,
+                    date: dateStr,
+                    timework: getRandomInt(1, 2)
+                }]
+            }
+            console.log(cashRegistersData)
+
+            for (let i = 0; i < cashRegistersData.length; i++) {
+                await app.dbQuery(makePull('cashRegisters', cashRegistersData[i]), (res: any) => {
+                    console.log(makePull('cashRegisters', cashRegistersData[i]))
+                    console.log(res)
+                })
+            }
+            //     DELETE FROM Cashiers
+
+        }
+
+        function makePull(tablename: any, fields: any){
+            let query = ''
+            console.log(fields)
+            let keys = Object.keys(fields)
+
+            Object.values(fields).forEach((value, index) => {
+                if (typeof value === "string") fields[keys[index]] = `\'${value}\'`
+            })
+
+            console.log(fields)
+            query = `INSERT INTO ${tablename} (${Object.keys(fields)})
+            VALUES 
+                (${Object.values(fields)})
+            ;`
+
+            return query
+        }
+        function makeCreate(tablename: any, fields: any){
+            let str = ''
+
+            let lastItem = Object.keys(fields)[Object.keys(fields).length-1];
+            for (const [key, value] of Object.entries(fields)) {
+                str += `${key} ${value}`
+                if(lastItem !== key) str += ', '
+            }
+
+
+            query = `CREATE TABLE ${tablename}(
+                ${str}
+            );`
+            console.log(query)
+
+            return query
+        }
+
+        // await pushCashRegistersData()
+
+            //     DELETE FROM cashRegisters
+
+
+
 
 
         let query = `SELECT *
@@ -116,7 +231,7 @@ class App {
             for (let i = 0; i < tables.length; i++){
                 const table = tables[i];
                 console.log(table.tablename)
-                await app.dbQuery(create(table),(res: any) => {
+                await app.dbQuery(makeCreate(table.tablename, table.fields),(res: any) => {
                     console.log(res)
                 })
             }
@@ -125,24 +240,6 @@ class App {
 
         })
 
-
-        function create(element: any){
-            let str = ''
-
-            let lastItem = Object.values(element.fields)[Object.keys(element.fields).length-1];
-            for (const [key, value] of Object.entries(element.fields)) {
-                str += `${key} ${value}`
-                if(lastItem !== value) str += ', '
-            }
-
-
-            query = `CREATE TABLE ${element.tablename}(
-                ${str}
-            );`
-            console.log(query)
-
-            return query
-        }
 
         function addShopsIDsToCashiers(){
             shopsData.forEach(shop => {
@@ -158,7 +255,9 @@ class App {
                 })
             })
         }
-
+        return {
+            pushCashRegistersData
+        }
 
     }
 
@@ -176,32 +275,31 @@ let dbData = {
 }
 
 let app = new App(dbData.user, dbData.host, dbData.database, dbData.password, dbData.port)
-// app.checkTables()
+// app.deleteTables()
 
 
 
 
 
-
-let query = `SELECT * FROM CASHIERS`
-query = `
-    INSERT INTO Cashiers(${Object.keys(tables[0].fields).join(',')})
-    VALUES ('1', 'Ivan', 'Shevchuk', 23);
-    `
+// let query = `SELECT * FROM CASHIERS`
 // query = `
-//     SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'cashiers';
+//     INSERT INTO Cashiers(${Object.keys(tables[0].fields).join(',')})
+//     VALUES ('1', 'Ivan', 'Shevchuk', 23);
 //     `
-
-
-app.dbQuery(query,(res: any) => {
-    console.log(query)
-    console.log(res.rows)
-
-
-
-    // app.deleteTables()
-
-})
+// // query = `
+// //     SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'cashiers';
+// //     `
+//
+//
+// app.dbQuery(query,(res: any) => {
+//     console.log(query)
+//     console.log(res.rows)
+//
+//
+//
+//     // app.deleteTables()
+//
+// })
 
 
 // interface users {
